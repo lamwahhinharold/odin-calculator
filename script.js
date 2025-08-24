c23acaebe14925ee698fd2704123ab36437c34b6
Harold Lam <lamwahhinharold@gmail.com>
2025-08-24 21:42:07 +0800

    Add operate function to invoke math operators functions

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNumber;
+let operator;
+let secondNumber;
+
 function add(x, y) {
   return x + y;
 }
@@ -12,4 +16,17 @@ function multiply(x, y) {
 
 function divide(x, y) {
   return x / y;
+}
+
+function operate(firstNumber, operator, secondNumber) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return subtract(firstNumber, secondNumber);
+    case "*":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+  }
 }
